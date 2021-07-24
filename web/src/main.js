9326224c3fe0109fc310b8da2d0144da4e9b9606
wei840222 <wei840222@gmail.com>
2021-07-24 12:55:15 +0800

    remove vuex

--- web/src/main.js
+++ web/src/main.js
@@ -1,9 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 import installVant from "./plugins/vant";
 
 const app = createApp(App);
 installVant(app);
-app.use(store).use(router).mount("#app");
+app.use(router).mount("#app");
