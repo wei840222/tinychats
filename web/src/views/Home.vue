ccdabf332341bb299b33fcd31e74ca97531c3d0d
wei840222 <wei840222@gmail.com>
2021-07-24 16:15:30 +0800

    create todo

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,30 +1,60 @@
 <template lang="pug">
-van-loading(v-if="loading" style="text-align:center;margin-top:10px;") Loading...
-van-cell(v-else v-for="(t, i) in todos" :key="t.id" :title="t.text")
+van-loading(v-if="loading", style="text-align: center; margin-top: 10px") Loading...
+van-cell(v-else, v-for="(t, i) in todos", :key="t.id", :title="t.text")
   template(#right-icon)
-    van-switch(v-model="doneSwitchState[t.id]" size="24")
+    van-switch(v-model="doneSwitchState[t.id]", size="24")
+van-field(v-model="createTodoState")
+  template(#button)
+    van-button(size="small", @click="createTodo") add
 </template>
 
 <script>
 import { ref } from "vue";
-import { useQuery, useResult } from "@vue/apollo-composable";
+import { useQuery, useResult, useMutation } from "@vue/apollo-composable";
 import gql from "graphql-tag";
+
+const LIST_TODOS = gql`
+  query listTodos {
+    todos {
+      id
+      text
+      done
+    }
+  }
+`;
 
 export default {
   name: "Home",
   setup() {
-    const { result, loading } = useQuery(gql`
-      query listTodos {
-        todos {
-          id
-          text
-          done
-        }
-      }
-    `);
+    const { result, loading } = useQuery(LIST_TODOS);
     const todos = useResult(result, [], (data) => data.todos);
     const doneSwitchState = ref({});
-    return { loading, todos, doneSwitchState };
+    const createTodoState = ref("");
+
+    const { mutate: createTodo } = useMutation(
+      gql`
+        mutation createTodo($text: String!) {
+          createTodo(input: { text: $text }) {
+            id
+            text
+            done
+          }
+        }
+      `,
+      () => ({
+        variables: {
+          text: createTodoState.value,
+        },
+        update: (cache, { data: { createTodo } }) => {
+          createTodoState.value = "";
+          let data = cache.readQuery({ query: LIST_TODOS });
+          data = JSON.parse(JSON.stringify(data));
+          data.todos.push(createTodo);
+          cache.writeQuery({ query: LIST_TODOS, data });
+        },
+      })
+    );
+    return { loading, todos, doneSwitchState, createTodoState, createTodo };
   },
 };
 </script>
